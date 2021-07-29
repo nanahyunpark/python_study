e485bc120bea13445830bc2b63c69a2e37d7303c
nanahyunpark <nahyun4576@gmail.com>
2021-07-29 17:34:17 +0900

    update 171.py

---
+++ 171.py
@@ -0,0 +1,15 @@
+import interest_rate
+
+def get_number(num):
+    result = 0
+    for i in range(1, num+1):
+        result += i
+    return result
+def main():
+    num = int(input())
+    number = get_number(num)
+    print(number)
+
+if __name__ == '__main__':
+    main()
+
--- interest_rate.py
+++ interest_rate.py
@@ -1,29 +1,30 @@
-# interest rate 최소공배수 단위로 배열 만들어서 계산값 집어넣어놔야함
-# (i_r[15] ** y//15 ) * (i_r[y%15])
+if __name__ == '__main__':
+    # interest rate 최소공배수 단위로 배열 만들어서 계산값 집어넣어놔야함
+    # (i_r[15] ** y//15 ) * (i_r[y%15])
 
-init_money, fund_year = map(int, input().split())
+    init_money, fund_year = map(int, input().split())
 
-#print(init_money, fund_year)
+    #print(init_money, fund_year)
 
-dp = [0 for i in range(fund_year+1)]
-dp[0] = init_money
-#print(dp)
+    dp = [0 for i in range(fund_year+1)]
+    dp[0] = init_money
+    #print(dp)
 
-for year in range(1, fund_year+1):
-    #print(year)
-    print(year, dp)
-    dp[year] = dp[year-1] * 1.05
-    
-    if year >= 3:
-        if dp[year] < dp[year-3] * 1.2:
-            dp[year] = dp[year-3] * 1.2
+    for year in range(1, fund_year+1):
+        #print(year)
+        print(year, dp)
+        dp[year] = dp[year-1] * 1.05
         
-    
-    if year >= 5:
-        if dp[year] < dp[year-5] * 1.35:
-            dp[year] = dp[year-5] * 1.35
-    
-    # dp[year] = int(dp[year])
+        if year >= 3:
+            if dp[year] < dp[year-3] * 1.2:
+                dp[year] = dp[year-3] * 1.2
+            
+        
+        if year >= 5:
+            if dp[year] < dp[year-5] * 1.35:
+                dp[year] = dp[year-5] * 1.35
+        
+        # dp[year] = int(dp[year])
 
-#print(dp)
-print(int(dp[fund_year]))
+    #print(dp)
+    print(int(dp[fund_year]))
